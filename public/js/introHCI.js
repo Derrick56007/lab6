523d84a4995b1cf777e3276bb60315d771b8110c
Derrick Liu <derrick56007@hotmail.com>
2019-02-14 09:44:05 -0800

    done

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -17,14 +17,34 @@ function initializePage() {
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
+
+
 function addProjectDetails(e) {
-	// Prevent following the link
-	e.preventDefault();
+    // Prevent following the link
+    e.preventDefault();
 
-	// Get the div ID, e.g., "project3"
-	var projectID = $(this).closest('.project').attr('id');
-	// get rid of 'project' from the front of the id 'project3'
-	var idNumber = projectID.substr('project'.length);
+    // Get the div ID, e.g., "project3"
+    var projectID = $(this).closest('.project').attr('id');
+    // get rid of 'project' from the front of the id 'project3'
+    var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+    console.log("User clicked on project " + idNumber);
+
+    var name = "/project/" + idNumber;
+    console.log( name );
+
+    $.get(name, addProject);
+}
+
+function addProject (result){
+    console.log(result);
+    var name = result['id'];
+    name = '#project'+name+' .thumbnail';
+    name = $(name).find('.details');
+
+    var projectHTML = '<img src="' + result['image'] + '" class="detailsImage">' +
+        '<p>' + result['title'] + '</p>' +
+        '<p><small>' + result['date'] + '</small></p>' +
+        '<p>' + result['summary'] + '</p>';
+    $(name).html(projectHTML);
 }
